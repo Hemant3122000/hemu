953cd8930119d881ce98a883fe42ee3c46d96f97
Hemant Kumar Meena <hemantk.meena.mec17@itbhu.ac.in>
2020-06-18 17:52:47 +0530

    template parameter N is removed and accessing methods added

--- hemu.h
+++ hemu.h
@@ -7,55 +7,67 @@ namespace hemu
 {
 	//Tensor class for storing dynamically allocated tensor which have elements of generic type
 	//T is type of elements and N is Number of dimensions
-	template<typename T, size_t N>
+	template<typename T>
 	class Tensor
 	{
 		//private data members for hemu::Tensor
 	private:
+		//Number of dimensions
+		size_t N;
 		//pointer to store shape of Tensor(on heap)
 		size_t* shape;
 
 		//pointer to store hemu::Tensor elements(on heap)
-		int* data;
+		T* data;
 	public:
 		//returns number of elements of Tensor
 		size_t numel() const
 		{
 			size_t n = 1;
-			for (size_t i = 0; i < N; i++)
+			for (size_t i = 0; i < this->N; i++)
 				n *= *(shape + i);
 			return n;
 		}
 
-		//returns shape of Tensor
-		const size_t* GetShape() const
-		{
-			return this->shape;
-		}
-
 		//default constructor for Tensor
-		Tensor()
-		{
-			this->shape = new size_t[N]{};
-			this->data = new T[this->numel()]{};
-		}
+		Tensor() = delete;
 
 		//constructor with parameters for Tensor
 		Tensor(std::initializer_list<size_t> shape)
 		{
-			this->shape = new size_t[N]{};
+			this->N = shape.size();
+			this->shape = new size_t[this->N]{};
 			std::copy(shape.begin(), shape.end(), this->shape);
 			this->data = new T[this->numel()]{};
-			
 		}
 
+		//constructor using shape pointer and Number of dimensions
+		Tensor(size_t* shape_ptr,size_t N)
+		{
+			this->N = N;
+			this->shape = new size_t[this->N]{};
+			std::copy_n(shape_ptr, this->N, this->shape);
+			this->data = new T[this->numel()]{};
+		}
 		//copy constructor
 		Tensor(const Tensor& src)
 		{
-			this->shape = new size_t[N]{};
-			std::copy_n(src.shape, N, src.shape);
+			this->N = src.N;
+			this->shape = new size_t[src.N]{};
+			std::copy_n(src.shape, src.N, this->shape);
 			this->data = new T[src.numel()]{};
 			std::copy_n(src.data, src.numel(), this->data);
+		}
+
+		//move constructor
+		Tensor(const Tensor&& src)
+		{
+			this->N = src.N;
+			this->shape = new size_t[src.N]{};
+			std::copy_n(src.shape, src.N, this->shape);
+			this->data = new T[src.numel()]{};
+			std::copy_n(src.data, src.numel(), this->data);
+			src.~Tensor();
 		}
 
 		//destructor
@@ -66,29 +78,39 @@ namespace hemu
 			delete[] this->shape;
 			this->shape = nullptr;
 		}
-		//[] for accessing elements of tensor
-		/*auto& operator[](size_t index)
+
+		//method to get pointer at indexed location
+		T* ptrat(std::initializer_list<size_t> index)
 		{
-			static size_t count = 0;
-			static size_t temp = 0;
-			std::vector<size_t> s = this->shape;
-			for (size_t i = 0; i <= count; i++)
-				s[i] = 1;
-			size_t n = 1;
-			for (size_t i : s)
-				n *= i;
+			size_t n = 0;
+			size_t n1 = 1;
+			for (int i = 0; i < index.size(); i++)
+			{
+				n1 = *(index.begin() + i);
+				for (int j = 0; j < (this->N - i - 1); j++)
+				{
+					n1 *= *(this->shape + 1 + i + j);
+				}
+				n += n1;
+			}
+			return this->data + n;
+		}
 
-			n += temp;
-			temp = n;
-			Tensor<T, N - 1> dst;
-			std::copy(this->data[temp+1], this->data[n], dst.data[0]);
-			std::copy(this->shape[count + 1], this->shape.end(), dst.shape[0]);
-			return dst;
-		}*/
-
-		void operator=(std::initializer_list<int> data)
+		//at method to access individual elements
+		T& at(std::initializer_list<size_t> index)
+		{
+			return *(this->ptrat(index));
+		}
+		//= overload to assign values to underlying flat array using initializer_list
+		void operator=(std::initializer_list<T> data)
 		{
 			std::copy(data.begin(), data.end(), this->data);
+		}
+
+		//= overload to assign values to underlying flat array using data pointer
+		void operator=( T* data_ptr)
+		{
+			std::copy_n(data_ptr, this->numel(), this->data);
 		}
 	};
 }
--- test.cpp
+++ test.cpp
@@ -4,7 +4,12 @@
 
 int main()
 {
-	hemu::Tensor<int,2> a({ 2,3 });
+	hemu::Tensor<int> a({ 2,3 });
 	a = { 1,2,3,4,5,6 };
+	auto b = a.at({ 1,1 });
+	std::cout << b << std::endl;
+	auto d(a);
+	d.at({ 1,1 }) = 7;
+	std::cout << d.at({ 1,1 });
 	return 0;
 }
